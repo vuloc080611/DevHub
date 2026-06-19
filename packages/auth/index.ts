294dbce275f4f4ee4ffff3785fa0576f625d9b5e
only4u <vuloc668165@gmail.com>
2026-06-19 08:51:33 +0700

    Update index.ts

--- packages/auth/index.ts
+++ packages/auth/index.ts
@@ -1,1 +1,30 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "@devhub/db";
+import type { NextAuthConfig } from "next-auth";
 
+const authConfig: NextAuthConfig = {
+  adapter: PrismaAdapter(db),
+  providers: [
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+};
+
+export const { handlers, signIn, signOut, auth } = NextAuth(authConfig);
