383c1dd939e77ccb43be204b71cf168106c18bb5
only4u <vuloc668165@gmail.com>
2026-06-23 12:42:56 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,8 @@
+import { redirect } from "next/navigation";
+import { auth } from "@devhub/auth";
 
+export default async function Home() {
+  const session = await auth();
+  if (session) redirect("/dashboard");
+  redirect("/login");
+}
